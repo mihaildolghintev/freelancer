aef54db3c05bbc7982a1ed5f3746f888fa84725e
misha dolghintev <yadolghintev@gmail.com>
2021-10-28 16:10:24 +0300

    Add User parameters

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'bulma-extensions-rails'
 gem 'bulma-rails'
+gem 'devise'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name])
+  end
 end
